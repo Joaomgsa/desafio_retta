fa5b77e50acbf629b6c58387b667e9ea2b98b97d
joaomgsa <joaomgsa@gmail.com>
2024-08-04 11:27:33 -0300

    alinhando treemap

--- services/GenerateTreeMap.py
+++ services/GenerateTreeMap.py
@@ -4,40 +4,45 @@ import plotly.express as px
 import pandas as pd
 
 
-class GenerateTreeMap:
-    """
-    Classe para gerar um treemap a partir de dados de medalhas.
-    """
+class GenerateTreeMap():
 
     def __init__(self):
         pass
-
-    def medals(self) -> dict:
-        """
-        Obtém os dados de medalhas a partir de uma API.
-
-        Returns:
-            dict: Dados de medalhas em formato de dicionário.
-            None: Se a resposta da API não for bem-sucedida.
-        """
+    
+    def medals() -> dict:
         try:
             api_url = os.getenv('API_BASE_URL')
-            if not api_url:
-                raise ValueError("API_BASE_URL não está definida nas variáveis de ambiente.")
-            
             response = requests.get(api_url)
-            response.raise_for_status()  # Levanta um erro para códigos de status 4xx/5xx
-
-            data = response.json()
-            print(data)
+            
+            if response.status_code == 200:
+                data = response.json()
+                print(data)
+            if response.status_code >= 300:
+                return None
+            
             return data
-
-        except requests.exceptions.RequestException as e:
-            print(f"Erro ao acessar a API: {e}")
-            return None
-        except ValueError as e:
-            print(f"Erro de configuração: {e}")
-            return None
+        
         except Exception as e:
-            print(f"Erro inesperado: {e}")
-            return None
+            return str(e)
+    
+    def handle(self):
+        
+        medals = GenerateTreeMap.medals()
+        
+        print(type(medals['data']))
+        
+        if medals is None:
+            return "Erro ao acessar a API"
+        
+        df = pd.DataFrame(medals['data'])
+        
+        fig = px.treemap(df, 
+                         path=['id', 'total_medals'],
+                         values='total_medals',
+                         color='total_medals',
+                         hover_data=['gold_medals','silver_medals','bronze_medals'],
+                         title='Quadro de Medalhas - Paris 2024')
+        fig.update_layout(margin=dict(t=50, l=25, r=25), title_x=0.5)
+
+        treemap = fig.to_html(full_html=False)
+        return treemap
